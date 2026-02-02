12e6da861e1f2e2536fee10f7e43365f8939a0a4
Eason <wu.6479@osu.edu>
2026-02-02 00:38:07 -0500

    Add ROS2-ready SO-ARM URDF, meshes, and RViz launch

---
+++ src/so_arm100_description/launch/display.launch.py
@@ -0,0 +1,35 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    pkg_name = 'so_arm100_description'
+    urdf_file = 'so101_new_calib.urdf'
+    pkg_share = get_package_share_directory(pkg_name)
+    urdf_path = os.path.join(pkg_share, 'urdf', urdf_file)
+
+    with open(urdf_path, 'r') as infp:
+        robot_desc = infp.read()
+
+    return LaunchDescription([
+        Node(
+            package='robot_state_publisher',
+            executable='robot_state_publisher',
+            name='robot_state_publisher',
+            output='screen',
+            parameters=[{'robot_description': robot_desc}],
+        ),
+        Node(
+            package='joint_state_publisher_gui',
+            executable='joint_state_publisher_gui',
+            name='joint_state_publisher_gui'
+        ),
+        Node(
+            package='rviz2',
+            executable='rviz2',
+            name='rviz2',
+            output='screen'
+        )
+    ])
+
